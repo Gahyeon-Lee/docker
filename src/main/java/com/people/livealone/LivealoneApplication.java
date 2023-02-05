a8da3b87090e2296ec6ee1def27554d86aa328c2
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-02-05 23:28:45 +0900

    initial commit

---
+++ src/main/java/com/people/livealone/LivealoneApplication.java
@@ -0,0 +1,13 @@
+package com.people.livealone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivealoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivealoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/people/livealone/LivealoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.livealone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivealoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
